04232c39e3a05dc640b1bd5bf78329878d057f3c
Lingbo Tang <lingbo19.tang@gmail.com>
2014-10-14 14:53:32 -0600

    Update SubmitQuestionTest.java

--- TestProj2Lingbo/src/android/support/v7/appcompat/test/SubmitQuestionTest.java
+++ TestProj2Lingbo/src/android/support/v7/appcompat/test/SubmitQuestionTest.java
@@ -12,12 +12,33 @@ import java.io.File;
 public class SubmitQuestionTest {
 	
 	Question question = new Question();
+	
+	Calender cal = Calender.getInstance();
+	cal.set(Calendar.YEAR, 2014);
+	cal.set(Calendar.MONTH, 10);
+	cal.set(Calendar.DAY_OF_MONTH, 1);
+	cal.set(Calendar.HOUR_OF_DAY, 1);
+	cal.set(Calendar.MINUTE, 1);
+	cal.set(Calendar.SECOND, 2);
+	java.util.Date date = cal.getTime();
+	String author = "Lingbo";
+	String text = "This is the answer.";
+	int cid = 123;
+	String filename = "lll.jpg";
+	question.addDate(date);
+	question.addAuthor(author);
+	question.addText(text);
+	question.addCid(cid);
+	question.addImage(filename);
+	assertNotNull(question.getText());
+	assertNotNull(question.getImage());
+	assertSame(question.getText(),textBody);
+	assertSame(question.getImage().getname(),filename);
+	
 	String title = "what is this?";
 	question.addtitle(title);
 	String textBody = "I have a question for this.";
 	question.addBody(textBody);
-	String filename = "kkk.jpg";
-	question.addFile(filename);
 	assertNotNull(question.gettitle());
 	assertNotNull(question.getBody());
 	assertNotNull(question.getFile());
