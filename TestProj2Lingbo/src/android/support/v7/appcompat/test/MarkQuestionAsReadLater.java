c0037597744519e4f3d372dd2dcdf7526fc7b462
Lingbo Tang <lingbo19.tang@gmail.com>
2014-10-14 10:37:52 -0600

    Update MarkQuestionAsReadLater.java

--- TestProj2Lingbo/src/android/support/v7/appcompat/test/MarkQuestionAsReadLater.java
+++ TestProj2Lingbo/src/android/support/v7/appcompat/test/MarkQuestionAsReadLater.java
@@ -1,20 +1,43 @@
 package android.support.v7.appcompat.test;
 
+import android.test.ActivityInstrumentationTestCase2;
+import java.security.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import junit.framework.TestCase;
+import android.view.View;
+import java.util.ArrayList;
+import java.io.File;
 
 public class MarkQuestionAsReadLater {
 	
 	
-	Topics rl = new Topics();
+	ReadLater rl = new ReadLater();
 	
 	Topics topics = new Topics();
 	
-	Question question  = topics.addQuestion("What is this?");
-	rl.getquestion(question).markAsReadLater();
-	assertTrue(rl.hasAsReadLater() == question);
+	Question question  = new Question();
 	
-	Question question2 = topics.addQuestion("What is this?");
-	assertTrue(r1.getquestion(question).makAsReadsLater() == question);
+	Calender cal = Calender.getInstance();
+	cal.set(Calendar.YEAR, 2014);
+	cal.set(Calendar.MONTH, 10);
+	cal.set(Calendar.DAY_OF_MONTH, 1);
+	cal.set(Calendar.HOUR_OF_DAY, 1);
+	cal.set(Calendar.MINUTE, 1);
+	cal.set(Calendar.SECOND, 2);
+	java.util.Date date = cal.getTime();
+	String author = "Lingbo";
+	String text = "This is the answer."
+	int cid = 123;
+	question.addDate(date);
+	question.addAuthor(author);
+	question.addText(text);
+	question.addCid(cid);
+	
+	rl.getquestion(question).markAsReadLater();
+	assertTrue(rl.hasAsReadLater());
+	assertSame(r1.getQuestion(question),question);
+	
 	
 	
 	public void TestRLCache() {
