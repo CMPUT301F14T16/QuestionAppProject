080da64f00d56eea7b3c03a7715e6c131e8f157e
Lingbo Tang <lingbo19.tang@gmail.com>
2014-10-17 22:43:56 -0600

    Update SubmitReplyTest.java

--- Archived/Project2Test/src/com/example/project2/test/SubmitReplyTest.java
+++ Archived/Project2Test/src/com/example/project2/test/SubmitReplyTest.java
@@ -11,38 +11,82 @@ import java.io.File;
 
 public class SubmitAnswerTest {
 	
-	Answer answer = new Answer();
-	Calender cal = Calender.getInstance();
-	cal.set(Calendar.YEAR, 2014);
-	cal.set(Calendar.MONTH, 10);
-	cal.set(Calendar.DAY_OF_MONTH, 1);
-	cal.set(Calendar.HOUR_OF_DAY, 1);
-	cal.set(Calendar.MINUTE, 1);
-	cal.set(Calendar.SECOND, 2);
-	java.util.Date date = cal.getTime();
-	String author = "Lingbo";
-	String text = "This is the answer."
-	int cid = 123;
-	Reply Reply = new Reply();
-	Reply.addDate(date);
-	Reply.addAuthor(author);
-	Reply.addText(text);
-	Reply.addCid(cid);
-	answer.addReply(reply);
-	assertNotNull(answer.getReplies());
-	assertSame(answer.getReplies(),reply);
+	public void SubmitQuestion() {
+		//Initialize the Question Input.
+		int r_id;
+		MainModel mainmodel = new MainModel();
+		mainModel.setUser("123@sample.com","userName");
+		mainmodel.addQuestion("title","what is this?").addReply("This is not dog");
+		assertTrue(mainmodel.getAllQuestion().getAllReplies().size()==1);
+		
 	
+		/*Calender cal = Calender.getInstance();
+		cal.set(Calendar.YEAR, 2014);
+		cal.set(Calendar.MONTH, 10);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 1);
+		cal.set(Calendar.MINUTE, 1);
+		cal.set(Calendar.SECOND, 2);
+		java.util.Date date = cal.getTime();
+		String author = "Lingbo";
+		String title = "What is this?";
+		String text = "This is the answer.";
+		int cid = 123;
+		String filename = "lll.jpg";
+		question.addDate(date);
+		question.addAuthor(author);
+		question.addTitle(title);
+		question.addText(text);
+		question.addCid(cid);
+		question.addImage(filename);
+		//Assert the question is not empty at first
+		//To test if we can parse them.
+		
+		assertNotNull(question.getDate());
+		assertNotNull(question.getAuthor());
+		assertNotNull(question.getText());
+		assertNotNull(question.getCid());
+		assertNotNull(question.getImage());*/
+		
+		//Assert if we can parse the correct value in the 
+		//question to submit
+		/*assertSame(question.getDate(),date);
+		assertSame(question.getAuthor(),author);
+		assertSame(question.getText().getTitle(),title);
+		assertSame(question.getText().getBody(),text);
+		assertSame(question.getCid(),cid);
+		assertSame(question.getImage().getname(),filename);*/
+	}
 	
-	public void TestCache() {
-		if (connection == 0) {
-			assertTrue(setReconnect());
+	//Pending is cache like container for the edited but unsubmitted content
+	//Once we have the connection, we will push up content to the view and update
+	//I have no idea what to do with the Android Cache
+	//So right now it's just a pseudocode like test.
+	//We come out the idea to use the quesiton_id,answer_id and reply_id to distinct
+	//the content, but since we are still discussing that, we just make it abstract in
+	//our test code.
+	//If there is anything already inside the list
+	public void TestPending() {
+		MainModel mainModel= new MainModel();
+		mainModel.setUser("123@sample.com","userName");
+		try{
+			mainModel.addQuestion("title","this is a question").addReply("This is not a cat");
 		}
+		catch (Exception e){
+			mainModel.addPending("title","this is a question");
+		}
+		Boolean connect=false;
+		while !connect{
+			try{
+				mainModel.pushPending();
+			}
+			else{
+				connect=true;
+			}
+		}
+		assertEqual(Mainmodel.haspending(), false);
+		assertNotNull(Mainmodel.getAllQuestion().getAllReplies());
 		
-		Cache cache = new Cache();
-		
-		cache.addAnswer(answer);
-		assertNotNull(Cache.getAnswer());
-		assertSame(answer.getReplies().getText(),text);
 	}
 
 }
