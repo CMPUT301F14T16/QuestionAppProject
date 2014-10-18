fc1187211f3ce9fd95a5a83977d7fe0a04b4c925
Lingbo Tang <lingbo19.tang@gmail.com>
2014-10-18 00:09:05 -0600

    Update MarkQuestionAsReadLater.java

--- Archived/Project2Test/src/com/example/project2/test/MarkQuestionAsReadLater.java
+++ Archived/Project2Test/src/com/example/project2/test/MarkQuestionAsReadLater.java
@@ -9,39 +9,61 @@ import android.view.View;
 import java.util.ArrayList;
 import java.io.File;
 
-public class MarkQuestionAsReadLater {
+public class MarkQuestionAsReadLater extends TestCase{
 	
+	public void testReadLater() {
 	
-	ReadLater rl = new ReadLater();
+		MainModel ReadLater = new MainModle();
+		
+		ReadLater.setUser("123@sample.com","userName");
+		ReadLater.addQuestion("title","what is this?");
+		assertTrue(ReadLater.getAllQuestion().size()==1);
 	
-	Topics topics = new Topics();
+		/*Calender cal = Calender.getInstance();
+		cal.set(Calendar.YEAR, 2014);
+		cal.set(Calendar.MONTH, 10);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 1);
+		cal.set(Calendar.MINUTE, 1);
+		cal.set(Calendar.SECOND, 2);
+		java.util.Date date = cal.getTime();
+		String author = "Lingbo";
+		String text = "This is the answer."
+		int cid = 123;
+		question.addDate(date);
+		question.addAuthor(author);
+		question.addText(text);
+		question.addCid(cid);
 	
-	Question question  = new Question();
-	
-	Calender cal = Calender.getInstance();
-	cal.set(Calendar.YEAR, 2014);
-	cal.set(Calendar.MONTH, 10);
-	cal.set(Calendar.DAY_OF_MONTH, 1);
-	cal.set(Calendar.HOUR_OF_DAY, 1);
-	cal.set(Calendar.MINUTE, 1);
-	cal.set(Calendar.SECOND, 2);
-	java.util.Date date = cal.getTime();
-	String author = "Lingbo";
-	String text = "This is the answer."
-	int cid = 123;
-	question.addDate(date);
-	question.addAuthor(author);
-	question.addText(text);
-	question.addCid(cid);
-	
-	rl.getquestion(question).markAsReadLater();
-	assertTrue(rl.hasAsReadLater());
-	assertSame(r1.getQuestion(question),question);
-	
+		rl.getquestion(question).markAsReadLater();
+		assertTrue(rl.hasAsReadLater());
+		assertSame(r1.getQuestion(question),question);*/
+	}
 	
 	
 	public void TestPending() {
-		if (connection == 0) {
+		
+		
+		MainModel ReadLater = new MainModel();
+		ReadLater.setUser("123@sample.com","userName");
+		try{
+			ReadLater.addQuestion("title","this is a question");
+		}
+		catch (Exception e){
+			ReadLater.addPending("title","this is a question");
+		}
+		Boolean connect=false;
+		while !connect{
+			try{
+				ReadLater.pushPending();
+			}
+			else{
+				connect=true;
+			}
+		}
+		assertEqual(ReadLater.haspending(), false);	
+		assertTrue(ReadLater.getAllSubmittedQuestion().size() == 1);
+		/*if (connection == 0) {
 			assertTrue(setReconnect());
 		}
 		Calender cal = Calender.getInstance();
@@ -61,7 +83,7 @@ public class MarkQuestionAsReadLater {
 		question.addCid(cid);
 		rl.getquestion(question).markAsReadLater();
 		assertTrue(rl.hasAsReadLater());
-		assertSame(r1.getQuestion(question),question);
+		assertSame(r1.getQuestion(question),question);*/
 	}
 	
 }
