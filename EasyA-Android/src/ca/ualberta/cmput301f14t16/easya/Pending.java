5d2a84dbe3d2a1140ade0742fe84ce1723637839
Lingbo Tang <lingbo19.tang@gmail.com>
2014-10-20 12:58:21 -0600

    Lingbo's Skeleton

---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Pending.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cmput301f14t16.easya;
+
+
+
+
+
+public class Pending {
+
+	public int qid;
+	public int aid;
+	public Content content;
+	public int getQid() {
+		return qid;
+	}
+	public int getAid() {
+		return aid;
+	}
+	public Content getContent() {
+		return content;
+	}
+	
+}
---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/User.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cmput301f14t16.easya;
+
+import java.util.List;
+
+public class User {
+	
+		public String userName;
+		public List<Integer> submittedContent;
+		
+		public String changeUserName(String newName) {
+			String newName1 = null;
+			newName = "abc";
+			return newName1;
+		}
+		
+		public void addContent(int id) {
+			Content content = new Content();
+		}
+}
