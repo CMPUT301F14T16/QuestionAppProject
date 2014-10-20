a76a7ce5649d8409dd12b87e7b3fb05b2dbf64aa
ann <ann@nat-5-137.uws.ualberta.ca>
2014-10-20 11:13:11 -0600

    Content and reply

---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Content.java
@@ -0,0 +1,18 @@
+package ca.ualberta.cmput301f14t16.easya;
+
+import java.sql.Date;
+
+public class Content {
+	protected Date date;
+	protected String text;
+	protected int cid;
+	protected User user;
+	protected String type;
+	
+	public String getType(){
+		
+	}
+	public int getID(){
+		
+	}
+}
---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Reply.java
@@ -0,0 +1,5 @@
+package ca.ualberta.cmput301f14t16.easya;
+
+public class Reply extends Content{
+
+}
