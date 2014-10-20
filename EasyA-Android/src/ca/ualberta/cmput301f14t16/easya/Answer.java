f6125a0991a990ae88cecbfd4eb722f9165bf021
Klinton Shmeit <shmeit@ualberta.ca>
2014-10-19 23:11:47 -0600

    Finished the  skeleton code

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Answer.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Answer.java
@@ -1,8 +1,18 @@
 package ca.ualberta.cmput301f14t16.easya;
 
+import java.util.Date;
+
 public class Answer {
 	
-	public Answer() {
+	public Answer(String title, String body, Date date) {
+		
+	}
+	
+	public int getAnswerID() {
+		return 0;
+	}
+	
+	public void setAnswerID() {
 		
 	}
 
