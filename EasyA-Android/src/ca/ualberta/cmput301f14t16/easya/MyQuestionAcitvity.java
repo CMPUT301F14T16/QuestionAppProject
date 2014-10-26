ca480ed71858fe64d8b366cd0878f8a4b6cdd28c
xiaolu1 <xiaolu1@ualberta.ca>
2014-10-25 19:15:08 -0600

    Extends activities

---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/MyQuestionAcitvity.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cmput301f14t16.easya;
+
+import android.os.Bundle;
+
+public class MyQuestionAcitvity extends MainActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+
+}
