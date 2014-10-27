0fc019a6d65c1aee1d7c96f4953d8f22aba3c347
cmput301-cauni <cauani.castro@ualberta.ca>
2014-10-27 01:15:15 -0600

    Creation of new Exceptions (NoInternet and NoClassTypeSelected)
    The former for when the app can't reach the internet and the latter for
    when we're trying to get the type from a Content but none has been
    specified (Just in case)

---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/NoInternetException.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cmput301f14t16.easya;
+
+public class NoInternetException extends Exception {
+	public NoInternetException(){
+		
+	}
+	
+	public NoInternetException(String message){
+		super(message);
+	}
+}
