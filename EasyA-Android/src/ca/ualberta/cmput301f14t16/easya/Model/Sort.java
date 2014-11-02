bde813533603a542d163b893d83d724a6fe3563b
xiaolu1 <xiaolu1@ualberta.ca>
2014-11-02 13:42:42 -0700

    Sort

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Sort.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Sort.java
@@ -1,11 +1,18 @@
 package ca.ualberta.cmput301f14t16.easya.Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Sort {
 	
 	public static void sortUpVote(boolean sortOrder,
 			ArrayList<Topic> topicList) {
+		Collections.sort(topicList, new Comparator<Topic>(){
+			public int compare(Topic topic1, Topic topic2){
+				return topic1.getUpVoteCount().compareTo(topic2.getUpVoteCount());
+			}
+		});
 	}
 	public static void pictureSort(boolean sortOrder,
 			ArrayList<Topic> topicList) {
