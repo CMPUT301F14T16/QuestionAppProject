4e704fbc40266d21c340687b1b285dc687b124f5
cmput301-cauni <cauani.castro@ualberta.ca>
2014-11-02 11:26:55 -0700

    Creation of QuestionList class
    The QuestionList class will be used to display the questions in a list

---
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/QuestionList.java
@@ -0,0 +1,45 @@
+package ca.ualberta.cmput301f14t16.easya.Model;
+
+/**
+ * Lightweight version of Question, for displaying in a list purpose only.
+ * @author Cauani
+ *
+ */
+public class QuestionList {
+	private String id;
+	private String title;
+	private String comments;
+	private String upvotes;
+	private boolean image;
+	public QuestionList(){
+		
+	}
+	
+	public QuestionList(String id, String title, String username, String comments, String upvotes, boolean image){
+		this.id = id;
+		this.title = title;
+		this.comments = comments;
+		this.upvotes = upvotes;
+		this.image = image;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getComments() {
+		return this.comments;
+	}
+
+	public String getUpvotes() {
+		return this.upvotes;
+	}
+	
+	public boolean getImage() {
+		return this.image;
+	}
+}
