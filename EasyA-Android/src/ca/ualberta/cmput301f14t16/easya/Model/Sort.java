2979b971d0634c948fefa76640abc4db305b0de2
Klinton Shmeit <shmeit@ualberta.ca>
2014-11-06 13:12:21 -0700

    fixed the sort tests to work with the methods and updated sort methods

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Sort.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Sort.java
@@ -26,13 +26,13 @@ public class Sort {
 	 *           
 	 */
 	public static void sortUpVote(boolean sortOrder,
-			List<Topic> questionList) {
-		Collections.sort(questionList, new Comparator<Topic>(){
-			public int compare(Topic topic1, Topic topic2){
+			List<QuestionList> questionList) {
+		Collections.sort(questionList, new Comparator<QuestionList>(){
+			public int compare(QuestionList questionList1, QuestionList questionList2){
 				// int cmp = a > b ? +1 : a < b ? -1 : 0;
-				// compares the 2 ints.
-				return topic1.getUpVoteCount() > topic2.getUpVoteCount() ? 1 :
-						topic1.getUpVoteCount() < topic2.getUpVoteCount() ? -1 : 0;
+				// compares the 2 upVotes.
+				return questionList1.getUpvotes().length() > questionList2.getUpvotes().length() ? 1 :
+					questionList1.getUpvotes().length() < questionList2.getUpvotes().length() ? -1 : 0;
 			}
 		});
 	}
@@ -47,11 +47,11 @@ public class Sort {
 	 *           
 	 */
 	public static void pictureSort(boolean sortOrder,
-			List<Topic> questionList) {
-		Collections.sort(questionList, new Comparator<Topic>(){
-			public int compare(Topic topic1, Topic topic2){
-				return topic1.hasPicture() && !topic2.hasPicture()? 1 :
-						!topic1.hasPicture() && topic2.hasPicture()? -1 : 0;
+			List<QuestionList> questionList) {
+		Collections.sort(questionList, new Comparator<QuestionList>(){
+			public int compare(QuestionList questionList1, QuestionList questionList2){
+				return questionList1.getImage() && !questionList2.getImage()? 1 :
+						!questionList1.getImage() && questionList2.getImage()? -1 : 0;
 			}
 		});
 	}
--- EasyA-AndroidTest/src/ca/ualberta/cmput301f14t16/easya/test/SortTest.java
+++ EasyA-AndroidTest/src/ca/ualberta/cmput301f14t16/easya/test/SortTest.java
@@ -3,79 +3,77 @@ package ca.ualberta.cmput301f14t16.easya.test;
 import java.util.ArrayList;
 
 import ca.ualberta.cmput301f14t16.easya.Model.Question;
+import ca.ualberta.cmput301f14t16.easya.Model.QuestionList;
 import ca.ualberta.cmput301f14t16.easya.Model.Sort;
 import ca.ualberta.cmput301f14t16.easya.Model.Topic;
 import junit.framework.TestCase;
 
-/**
- * 
- * @author Klinton Shmeit
- * 
- * this tests covers the 
- * use cases of SortUpVote, 
- * DateSort, and PictureSort.
- * 
- */
+//This tests covers the 
+//use cases of SortUpVote, 
+//DateSort, and PictureSort.
+//These sorts all belong to
+//the Sort class. 
+
 public class SortTest extends TestCase {
 	
 	public void testSortUpvote() {
 		boolean sortOrder;
 		Question q = new Question();
-		ArrayList<Topic> listTopicTest =  new ArrayList<Topic>();
+		ArrayList<QuestionList> listTopicTest =  new ArrayList<QuestionList>();
 
 		//highest to lowest sort.
 		sortOrder = true;
 			Sort.sortUpVote(sortOrder, listTopicTest);
 			for(int i = 0; i < listTopicTest.size(); i++){
-				assertTrue(listTopicTest.get(i).getUpVoteCount() >= listTopicTest.get(i++).getUpVoteCount());
+				assertTrue(listTopicTest.get(i).getUpvotes().length() >= listTopicTest.get(i++).getUpvotes().length());
 			}
 		//lowest to highest sort.
 		sortOrder = false;
 			Sort.sortUpVote(sortOrder, listTopicTest);
 			
 			for(int i = 0; i < listTopicTest.size(); i++){
-				assertTrue(listTopicTest.get(i).getUpVoteCount() <= listTopicTest.get(i++).getUpVoteCount());
+				assertTrue(listTopicTest.get(i).getUpvotes().length() <= listTopicTest.get(i++).getUpvotes().length());
 			
 			}
 	}
 	
 	public void DateSortTest() {
 		boolean beforeOrAfter;
-		ArrayList<Topic> listTopicsTest = new ArrayList<Topic>();
+		ArrayList<QuestionList> questionListTest = new ArrayList<QuestionList>();
 		//sorts from Topics with date most recent to oldest.
 		beforeOrAfter = true;
-		Sort.dateSort(beforeOrAfter, listTopicsTest);
-			for(int i = 0; i < listTopicsTest.size(); i++){
-				assertTrue(listTopicsTest.get(i).getDate().before(listTopicsTest.get(i++).getDate()) );
+		Sort.dateSort(beforeOrAfter, questionListTest);
+			for(int i = 0; i < questionListTest.size(); i++){
+				assertTrue(questionListTest.get(i).getDate().before(questionListTest.get(i++).getDate()) );
 			}
 		
 		//sorts from oldest to most recent
 		beforeOrAfter = false;
-		Sort.dateSort(beforeOrAfter, listTopicsTest);
-			for(int i = 0; i < listTopicsTest.size(); i++){
-				assertTrue(listTopicsTest.get(i).getDate().after(listTopicsTest.get(i++).getDate()) );
+		Sort.dateSort(beforeOrAfter, questionListTest);
+			for(int i = 0; i < questionListTest.size(); i++){
+				assertTrue(questionListTest.get(i).getDate().after(questionListTest.get(i++).getDate()) );
 			}
 		}
 	
 	public void PictureSortTest() {
 		boolean sortOrder;
 		
-		ArrayList<Topic> listTopicsTest = new ArrayList<Topic>();
+		ArrayList<QuestionList> questionListTest = new ArrayList<QuestionList>();
 		
 		//sorts from Topics with pictures to Topics without.
 		sortOrder = true;
-		Sort.pictureSort(sortOrder, listTopicsTest);
+		Sort.pictureSort(sortOrder, questionListTest);
 		//Assuming that the Topics are sorted from has a to doesn't have a picture,
 		//assert false if a previous picture doesn't have one but later one does. 
-			for(int i = 0; i < listTopicsTest.size(); i++){
-				if(listTopicsTest.get(i).hasPicture() == false){
-				assertFalse(listTopicsTest.get(i++).hasPicture() == true);
+			for(int i = 0; i < questionListTest.size(); i++){
+				if(questionListTest.get(i).getImage() == false){
+				assertFalse(questionListTest.get(i++).getImage() == true);
 				}
 			}
 		sortOrder = false;
-			for(int i = 0; i < listTopicsTest.size(); i++){
-				if(listTopicsTest.get(i).hasPicture() == true){
-				assertFalse(listTopicsTest.get(i++).hasPicture() == false);
+			for(int i = 0; i < questionListTest.size(); i++){
+				if(questionListTest.get(i).getImage() == true){
+				assertFalse(questionListTest.get(i++).getImage() == false);
 				}
 			}
 		}
