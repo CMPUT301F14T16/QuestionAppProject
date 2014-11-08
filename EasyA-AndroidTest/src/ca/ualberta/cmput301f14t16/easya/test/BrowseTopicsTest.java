8492b542c4cb461a72f30ea62bd46e7dc12e6c3f
Klinton Shmeit <shmeit@ualberta.ca>
2014-11-07 19:41:08 -0700

    new content creation test class

--- EasyA-AndroidTest/src/ca/ualberta/cmput301f14t16/easya/test/BrowseTopicsTest.java
+++ EasyA-AndroidTest/src/ca/ualberta/cmput301f14t16/easya/test/BrowseTopicsTest.java
@@ -33,22 +33,6 @@ public class BrowseTopicsTest extends TestCase {
 		e.printStackTrace();
 	}
 	assertNotNull(ql);
-	//use one topic for the test. this topic will be gotten from a list of topics.
-	Question question = new Question("Q title test", "Q body test", "Q author id test");
-	Answer answer = new Answer("A body test", "A author id test");
-	Reply reply = new Reply("R body test", "R author id test");
-	User user = new User();
-	//adds the question of the topic to an array of saved questions
-	question.addAnswer(answer);
-	question.addReply(reply);
-	question.setFavourite(user);
-	answer.setFavourite(user);
-	answer.addReply(reply);
-	//tests if answer added to question is correct
-	assertTrue(question.getAnswerCount() == 1);
-	//smoke test code reply and answers.
-	assertTrue(question.getReplies().get(0).getId().equals(reply.getId()));
-	assertTrue(question.getAnswers().get(0).equals(answer));
 	//Topic Previews (not yet tested for part 3 as browse is not yet implemented)
 	//User will choose what topics he/she would like to browse if he has Internet connectivity.
 	//this is assuming topics is in the database. The user will select the topic he/she
