1bb6dba53c720f528399fc129258e9e762c78ddf
Lingbo Tang <lingbo19.tang@gmail.com>
2014-11-18 19:23:56 -0700

    Update Answer.java

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Answer.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Answer.java
@@ -33,7 +33,7 @@ public class Answer extends Topic {
 	public Answer(String body, String authorId) {
 		super(body, authorId);
 	}	
-	public Answer(String body, byte[] bitmap, String authorId) {
+	public Answer(String body,byte[] bitmap, String authorId) {
 		super(body, bitmap, authorId);
 	}	
 }
