d556888fe3f0b11bf150ca923650e98d9e88a104
Klinton Shmeit <shmeit@ualberta.ca>
2014-11-21 18:56:50 -0700

    geo test

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Sort.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/Sort.java
@@ -75,8 +75,8 @@ public class Sort {
 		Collections.sort(questionList, new Comparator<QuestionList>() {
 			public int compare(QuestionList questionList1,
 					QuestionList questionList2) {
-				return questionList1.getImage() && questionList2.getImage() ? -1
-						: questionList1.getImage() && questionList2.getImage() ? 1
+				return questionList1.getImage() && !questionList2.getImage() ? -1
+						: !questionList1.getImage() && questionList2.getImage() ? 1
 								: 0;
 			}
 		});
