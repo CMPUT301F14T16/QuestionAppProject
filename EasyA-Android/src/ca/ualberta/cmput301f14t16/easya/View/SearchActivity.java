978f8a42ed4b1c97ab5d21af453c052e6d972327
cmput301-cauni <cauani.castro@ualberta.ca>
2014-11-24 14:51:56 -0700

    Updated layout for search
    Can still add a few more things, but will leave for later.

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/View/SearchActivity.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/View/SearchActivity.java
@@ -1,27 +1,28 @@
 package ca.ualberta.cmput301f14t16.easya.View;
 
 import java.util.List;
-
 import ca.ualberta.cmput301f14t16.easya.R;
-import ca.ualberta.cmput301f14t16.easya.Controller.ATasks.getQuestionListTask;
 import ca.ualberta.cmput301f14t16.easya.Controller.ATasks.searchQuestionTask;
 import ca.ualberta.cmput301f14t16.easya.Model.MainModel;
 import ca.ualberta.cmput301f14t16.easya.Model.QuestionList;
-import ca.ualberta.cmput301f14t16.easya.Model.Sort;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class SearchActivity extends MasterActivity {
 	private String query;
+	private View searchParams;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		position = 4;		
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		mDrawerToggle.setDrawerIndicatorEnabled(false);
-		
 		Intent intent = getIntent();
 		try{
 			this.query = intent.getStringExtra(SearchManager.QUERY);
@@ -31,17 +32,19 @@ public class SearchActivity extends MasterActivity {
 	    }
     }
 	
-	private void searchSetup(){
-		//TODO: add search layout parameters
-		//TODO: create layout parameters as a xml file
-		//TODO: inflate it here and move the list downwards
+	private void searchSetup(int size){
+		if (searchParams != null)
+			((ListView)findViewById(R.id.question_list)).removeHeaderView(searchParams);
+		searchParams = getLayoutInflater().inflate(R.layout.search_view_fragment, (ViewGroup)findViewById(R.id.question_list), false);
+		TextView tx = (TextView)searchParams.findViewById(R.id.search_fragment_hits);
+		tx.setText(String.valueOf(size));
+		((ListView)findViewById(R.id.question_list)).addHeaderView(searchParams);		
 	}
 	
 	@Override
 	protected void startUpdate() {
 		((TextView) findViewById(R.id.drawer_username)).setText(MainModel
-				.getInstance().getCurrentUser().getUsername());
-		searchSetup();
+				.getInstance().getCurrentUser().getUsername());		
 		(new searchQuestionTask(this, this, this.query)).execute();
 	}
 	
@@ -50,6 +53,8 @@ public class SearchActivity extends MasterActivity {
 	public void update(List<QuestionList> lst) {
 		super.update(lst);
 		stopAnimateSync();
+		searchSetup(lst.size());
+		//TODO: if no results, display the no-results-found page
 	}
 
 }
