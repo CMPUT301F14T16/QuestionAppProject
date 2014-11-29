732e8ee8e26a0ebd24b0f065886c230121c4120c
commande <commande@ualberta.ca>
2014-11-28 17:45:52 -0700

    cleaning up older unused directories. Still accessible through version control history.

--- EasyA-Android-Studio/app/src/main/java/ca/ualberta/cmput301f14t16/easya/ESPostResponse.java
+++
@@ -1,9 +0,0 @@
-package ca.ualberta.cmput301f14t16.easya;
-
-public class ESPostResponse {
-	public String _index;
-	public String _type;
-	public String _id;
-	public int _version;
-	public boolean created;
-}
