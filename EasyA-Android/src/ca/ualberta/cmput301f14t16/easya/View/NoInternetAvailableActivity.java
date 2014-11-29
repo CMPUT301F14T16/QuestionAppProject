c33790fd476196c4494fc23b10bd47b9e25e998b
xiaolu1 <xiaolu1@ualberta.ca>
2014-11-28 20:18:41 -0700

    No internet page works fine

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/View/NoInternetAvailableActivity.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/View/NoInternetAvailableActivity.java
@@ -33,11 +33,11 @@ public class NoInternetAvailableActivity extends Activity {
 		}
 	}
 
-	/*
+	
 	public void AddNewAnswer(View v){
 		Intent i = new Intent(v.getContext(), SubmitAnswerActivity.class);
 		i.putExtra(GeneralHelper.AQUESTION_KEY, qId);
 		this.startActivity(i);
 	}
-	*/
+
 }
