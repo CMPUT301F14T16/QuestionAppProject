85ae70d563116f8d90a2055ff2692752e300f877
Stephane Drouin-Moreland <sdrouinmoreland@gmail.com>
2014-11-28 19:42:41 -0700

    commented some bit

--- EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/GeoCoder.java
+++ EasyA-Android/src/ca/ualberta/cmput301f14t16/easya/Model/GeoCoder.java
@@ -6,89 +6,101 @@ import ca.ualberta.cmput301f14t16.easya.Model.Data.ContextProvider;
 import android.location.Geocoder;
 import android.location.Address;
 
-
+/**
+ * Provides various functions that interpret lat/lon coordinates, finding near
+ * addresses or locations, and formatting the result.
+ */
 public class GeoCoder {
-	public static String toAdress(double latitude, double longitude){
+	/**
+	 * Takes a lat/lon coordinate pair and finds the nearest address.
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @return The nearest address to the provided lat/lon coordinates.
+	 */
+	public static String toAdress(double latitude, double longitude) {
 		String myAddress;
-		Geocoder geocoder = new Geocoder(ContextProvider.get(), Locale.getDefault());
+		Geocoder geocoder = new Geocoder(ContextProvider.get(),
+				Locale.getDefault());
 		List<Address> addresses;
-		//StringBuilder strReturnedAddress = new StringBuilder("");
-		try{
+		// StringBuilder strReturnedAddress = new StringBuilder("");
+		try {
 			addresses = geocoder.getFromLocation(latitude, longitude, 1);
-			if (addresses != null && addresses.size()>0){
-				Address returnedAddress =addresses.get(0);
+			if (addresses != null && addresses.size() > 0) {
+				Address returnedAddress = addresses.get(0);
 				/*
-				for (int i=0;i<returnedAddress.getMaxAddressLineIndex();i++){
-					strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
-				}
-				myAddress=strReturnedAddress.toString();*/
+				 * for (int i=0;i<returnedAddress.getMaxAddressLineIndex();i++){
+				 * strReturnedAddress
+				 * .append(returnedAddress.getAddressLine(i)).append("\n"); }
+				 * myAddress=strReturnedAddress.toString();
+				 */
 				myAddress = formatAddress(returnedAddress);
+			} else {
+				myAddress = "";
 			}
-			else{
-				myAddress="";
-			}
-		}
-		catch (Exception e){
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			myAddress="";
+			myAddress = "";
 		}
 		return myAddress;
 	}
-	
-	private static String formatAddress(Address a){
+
+	private static String formatAddress(Address a) {
 		StringBuilder sb = new StringBuilder();
 		String aux = "";
 		aux = a.getLocality();
-		sb.append(aux != null ? (aux + ", ") : (a.getSubAdminArea() != null) ? (a.getSubAdminArea() + ", ") : "");
+		sb.append(aux != null ? (aux + ", ")
+				: (a.getSubAdminArea() != null) ? (a.getSubAdminArea() + ", ")
+						: "");
 		aux = a.getAdminArea();
 		sb.append(aux != null ? (aux + "/") : "");
 		sb.append(aux != null ? a.getCountryCode() : a.getCountryName());
 		return sb.toString();
 	}
-	
-	public static double[] toLatLong(String strAddress){
-		Geocoder geocoder = new Geocoder(ContextProvider.get(), Locale.getDefault());
+
+	public static double[] toLatLong(String strAddress) {
+		Geocoder geocoder = new Geocoder(ContextProvider.get(),
+				Locale.getDefault());
 		List<Address> addresses;
 		double[] returnLatLong = new double[2];
-		try{
+		try {
 			addresses = geocoder.getFromLocationName(strAddress, 1);
-			if (addresses != null && addresses.size()>0){
-				Address returnedAddress =addresses.get(0);
+			if (addresses != null && addresses.size() > 0) {
+				Address returnedAddress = addresses.get(0);
 				returnLatLong[0] = returnedAddress.getLatitude();
 				returnLatLong[1] = returnedAddress.getLongitude();
 			}
-		}
-		catch (Exception e){
-			return new double[]{0.0,0.0};
+		} catch (Exception e) {
+			return new double[] { 0.0, 0.0 };
 		}
 		return returnLatLong;
 	}
 
+	public static double toFindDistance(double[] coordinate1,
+			double[] coordinate2) {
+		double R = 6371000; // m
+		double dLat = Math.toRadians(coordinate1[0] - coordinate2[0]);
+		double dLon = Math.toRadians(coordinate1[1] - coordinate2[1]);
+		double lat1 = Math.toRadians(coordinate1[0]);
+		double lat2 = Math.toRadians(coordinate2[0]);
 
-	public static double toFindDistance(double[] coordinate1, double[] coordinate2){
-        double R = 6371000; // m
-        double dLat = Math.toRadians(coordinate1[0] - coordinate2[0]);
-        double dLon = Math.toRadians(coordinate1[1] - coordinate2[1]);
-        double lat1 = Math.toRadians(coordinate1[0]);
-        double lat2 = Math.toRadians(coordinate2[0]);
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2)
+				* Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double d = R * c;
 
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2)
-                * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double d = R * c;
-
-        return d;
+		return d;
 	}
-	
-	public static String coordinatesToString(double[] coord){
-		try{
+
+	public static String coordinatesToString(double[] coord) {
+		try {
 			return String.valueOf(coord[0]) + ";" + String.valueOf(coord[1]);
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			return "";
 		}
 	}
-	
-	public static double[] coordinatesFromString(String coord){
+
+	public static double[] coordinatesFromString(String coord) {
 		double[] dcoord = new double[2];
 		dcoord[0] = Double.valueOf((coord.split(";")[0]));
 		dcoord[1] = Double.valueOf((coord.split(";")[1]));
