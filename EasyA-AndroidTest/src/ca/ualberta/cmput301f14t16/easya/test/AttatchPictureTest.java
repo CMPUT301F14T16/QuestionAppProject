007003c2a80c6f258a3122e2660f1b0a421bc6d5
Lingbo Tang <lingbo19.tang@gmail.com>
2014-11-30 16:52:41 -0700

    Update AttatchPictureTest.java

--- EasyA-AndroidTest/src/ca/ualberta/cmput301f14t16/easya/test/AttatchPictureTest.java
+++ EasyA-AndroidTest/src/ca/ualberta/cmput301f14t16/easya/test/AttatchPictureTest.java
@@ -1,57 +1,108 @@
 package ca.ualberta.cmput301f14t16.easya.test;
 
-import java.io.ByteArrayInputStream;  
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-
+import java.io.IOException;
+import java.util.ArrayList;  
+import ca.ualberta.cmput301f14t16.easya.Model.Data.*;
+import java.util.Calendar;
+import java.util.List;
+import ca.ualberta.cmput301f14t16.easya.R;
+import ca.ualberta.cmput301f14t16.easya.Controller.ATasks.searchQuestionTask;
+import ca.ualberta.cmput301f14t16.easya.Model.Answer;
+import ca.ualberta.cmput301f14t16.easya.Model.MainModel;
+import ca.ualberta.cmput301f14t16.easya.Model.Question;
+import ca.ualberta.cmput301f14t16.easya.Model.QuestionList;
+import ca.ualberta.cmput301f14t16.easya.Model.User;
+import ca.ualberta.cmput301f14t16.easya.View.MainView;
+import ca.ualberta.cmput301f14t16.easya.View.SearchActivity;
+import android.app.ProgressDialog;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.util.Base64;
-import ca.ualberta.cmput301f14t16.easya.Model.Answer;
-import ca.ualberta.cmput301f14t16.easya.Model.Question;
+import android.os.AsyncTask;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 import junit.framework.TestCase;
-import ca.ualberta.cmput301f14t16.easya.View.*;
 
-public class AttatchPictureTest extends TestCase {
-  // To test all the attached pictures in all
-	// class type of object.
+public class SearchActivityTest extends ActivityInstrumentationTestCase2<SearchActivity> {
+	
+	
 	private Context ctx;
+	private ProgressDialog pd;
+	private MainView<List<QuestionList>> v;
+	private String QId;
+	private String AId;
+	private String QId2;
+	private String AId2;
+	private String email;
+	private String username;
 	
+	private List<QuestionList> lst;
+	private List<QuestionList> rst;
+	private ESClient esclient;
 	
-	public void testAttatchPictureTest () {
-		
+	public SearchActivityTest() {
+		super(SearchActivity.class);
+	}
 
-		byte[] bytebitmap;
+	public void testSearchActivityTest() {
+		ESClient esclient = new ESClient();
+		Question q1 = new Question("Title Submission Test", "Body of Question", "test@ualberta.ca");
+		Answer a1 = new Answer("Body of answer", "someone@ualberta.ca");
+		Question q2 = new Question("Different", "Different", "test2@ualberta.ca");
+		Answer a2 = new Answer("Different", "someone2@ualberta.ca");
+		q1.addAnswer(a1);
+		q2.addAnswer(a2);
+		QId = q1.getId();
+		AId = a1.getId();
+		QId2 = q2.getId();
+		AId2 = a2.getId();
+		
+		
+		email = "lingbo19.tang@gmail.com";
+		username = "lingbo747";
+		String query = "Title Submission Test";
+		User user = new User(email, username);
+		String userid = user.getId();
+		Calendar date = Calendar.getInstance();
 		double[] coordinates = new double[2];
 		coordinates[0] = 52.45;
 		coordinates[1] = 13.37;
 		String location = "Berlin German";
-		Intent data = new Intent();
-		Uri selectedImageUri = data.getData();
-		String[] projection = { MediaStore.Images.Media.DATA};
-		try { 
-			Cursor cursor = ctx.getApplicationContext().getContentResolver().query(selectedImageUri, projection, null, null, null);
-			int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-			cursor.moveToFirst();
-			String imagepath = cursor.getString(column_index);
-			Bitmap bitmap=BitmapFactory.decodeFile(imagepath);
-			ByteArrayOutputStream stream = new ByteArrayOutputStream();
-			bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-			byte[] byteArray = stream.toByteArray();
-			bytebitmap = Base64.encode(byteArray, 1);
-			Question q1 = new Question("What are you doing?","This is what I'm doing",bytebitmap,"lingbo@ualberta.ca");
-			Answer a1 = new Answer("This is it",bytebitmap,"lingbo@ualberta.ca",coordinates,location);
-			assertTrue(q1.getImage().equals(bytebitmap));
-			assertTrue(a1.getImage().equals(bytebitmap));
+		QuestionList qst = new QuestionList(QId, "Title Submission Test", username, userid,
+				"Body of answer", 1, false, date, coordinates, location);
+		QuestionList qst2 = new QuestionList(QId2, "Different", username, userid,
+				"Different", 1, false, date, coordinates, location);
+		//List<QuestionList> lst = new ArrayList<QuestionList>();
+		//lst.add(qst);
+		//lst.add(qst2);
+		List<QuestionList> rst = new ArrayList<QuestionList>();
+		rst.add(qst);
+		List<QuestionList> rst2 = new ArrayList<QuestionList>();
+		rst2.add(qst2);
+		
+		try{
+			assertTrue(esclient.submitQuestion(q1));
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
 			
-		}catch (NullPointerException e) {
+		Intent intent = new Intent();
+		intent.putExtra((SearchManager.QUERY).trim(),"Title Submission Test");
+		query = intent.getStringExtra((SearchManager.QUERY).trim());
+		setActivityIntent(intent);
+		SearchActivity sa = getActivity();
+		try{
+			lst = esclient.searchQuestionListsByQuery(query, 1);
+		}catch (IOException ex) {
 			assertFalse(false);
 		}
+		
+		assertNotNull(lst);
+		assertFalse(lst.get(0).equals(qst2));
+		assertEquals(lst.get(0).getTitle(),"Title Submission Test");
+		
+		
+		
 		
 	}
 }
